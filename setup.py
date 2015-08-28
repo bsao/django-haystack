1259eb752774d337e820a36f6e415793beb3ee86
bsao <robson.junior@vivareal.com>
2015-08-28 15:50:35 -0300

    another try to solve race problemas

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name='django-haystack',
-    version='2.4.0-bsao',
+    version='2.4.0.1-bsao',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
